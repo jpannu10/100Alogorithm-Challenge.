bcc735e09ebcb4e8f2e0f366186c09988b7d0e3a
Karan <jpannu10@gmail.com>
2021-02-16 13:37:40 -0500

    Update 100algoChallenge.js

--- 100algoChallenge.js
+++ 100algoChallenge.js
@@ -36,10 +36,37 @@ function addDigits( inputNums ) {
 }
 console.log(addDigits(1117))
 
-/*    */
 
 
-/*    */
+/*   Adjacent Element Product    */
+
+function adjacentElementsProducts(inputArray) {
+    let maxProduct = 0;
+    for (let i = 0; i < inputArray.length; i++) {
+        let temp = (inputArray[i] * inputArray[i + 1]);
+        maxProduct = temp > maxProduct ? temp : maxProduct;
+    }
+    return maxProduct;
+}
+console.log(adjacentElementsProducts([3, 6, 100, -2, -5, 7 ,100 , 3, 2]));
+
+
+/*  longestStringOutput  */
+function longestStringOutput(arrayInput) {
+
+    //Getting Longest Length.
+    let longestLength = 0;
+    arrayInput.forEach(element => longestLength = element.length > longestLength ? element.length : longestLength);
+
+    let outputinMatchedLongestLength = [];
+    //Matching Longest Length.
+    outputinMatchedLongestLength = arrayInput.filter(item => item.length === longestLength);
+
+    return longestLength;
+}
+console.log(longestStringOutput(['abs', 'jask','0273402397' ,'fd', 'yoh', 'we', '62nn34']));
+
+
 
 
 /*    */
